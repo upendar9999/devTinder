01579184989b9fd6f26515758a57c59a7c5f6ba5
upendar9999 <upendar.reddy.musuku@accenture.com>
2024-10-28 20:16:31 +0530

    writing-auth-middlewares

--- src/app.js
+++ src/app.js
@@ -2,25 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user",(req,res,next)=>{
-   console.log("Hellow World");
-   next();
-},(req,res,next)=>{
-   console.log("Hellow World 2");
-   next();
-},(req,res,next)=>{
-    console.log("Hellow World 3");
-    next();
-},(req,res,next)=>{
-    console.log("Hellow World 4");
-    // res.send("Response 4 !!");
-    next(); 
-},(req,res,next)=>{
-    console.log("Hellow World 5");
-    res.send("Response 5 !!");
-    
-}
-);
+const { adminAuth , userAuth} = require("./middlewares/auth");
+
+app.use("/admin",adminAuth);
+
+app.use("/user/login",(req,res)=>{
+    res.send("User Logged in successfully");
+})
+app.use("/user/data",userAuth,(req,res)=>{
+    res.send("User Data Sent");
+})
+app.get("/admin/getAllData",(req,res)=>{
+    res.send("All Data Sent");
+});
+
+app.get("/admin/deleteUser",(req,res)=>{
+    res.send("Delete a User");
+});
+
+
 app.listen(9999,()=>{
     console.log("Server is succesfully listening on port 9999..");
 });
