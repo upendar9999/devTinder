f4ab7773be711e5f70cc4c2287dd640d7784a287
upendar9999 <upendar.reddy.musuku@accenture.com>
2024-10-28 18:52:29 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -2,42 +2,25 @@ const express = require("express");
 
 const app = express();
 
-
-// // This will only handle get call to /user
-app.get("/user/:userId/:name/:password",(req,res)=>{
-    console.log(req.params);
-    res.send({firstName:"Upendar",lastName:"Musuku"});
-});
-
-
-// app.post("/user",(req,res)=>{
-//     console.log("Save the data to the database");
-//     res.send("Data successfully saved to the database");
-// });
-
-// app.delete("/user",(req,res)=>{
+app.get("/user",(req,res,next)=>{
+   console.log("Hellow World");
+   next();
+},(req,res,next)=>{
+   console.log("Hellow World 2");
+   next();
+},(req,res,next)=>{
+    console.log("Hellow World 3");
+    next();
+},(req,res,next)=>{
+    console.log("Hellow World 4");
+    // res.send("Response 4 !!");
+    next(); 
+},(req,res,next)=>{
+    console.log("Hellow World 5");
+    res.send("Response 5 !!");
     
-//     res.send("Data successfully deleted from the database");
-// });
-// app.get("/hello",(req,res)=>{
-
-//     res.send("Hello hello hello");
-// });
-
-// app.get("/hello/2",(req,res)=>{
-//     res.send("Hello second");
-// });
-
-// this will match all HTTP method API calls to /test
-
-// app.get("/test",(req,res)=>{
-//     res.send("Hello from the server");
-// });
-
-// app.use("/",(req,res)=>{
-//     res.send("Hello from dashboard");
-// });
-
+}
+);
 app.listen(9999,()=>{
     console.log("Server is succesfully listening on port 9999..");
 });
