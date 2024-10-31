2dcd606b865c22462876cf0e6feff0815bb2cc46
upendar9999 <upendar.reddy.musuku@accenture.com>
2024-10-31 15:59:16 +0530

    schema validations

--- src/app.js
+++ src/app.js
@@ -70,11 +70,14 @@ app.patch("/user",async (req,res)=>{
     const userId = req.body.userId;
     const data = req.body;
     try{
-         await User.findByIdAndUpdate({_id:userId},data);
+         await User.findByIdAndUpdate({_id:userId},data,{
+            returnDocument:"after",
+            runValidators:true,
+         });
         res.send("User updated successfully");
 
     }catch(err){
-        res.status(400).send("Something went wrong");
+        res.status(400).send("Update failed:"+ err.message);
     }
 
 });
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,50 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
+        minLength:4
     },
     lastName:{
         type:String
     },
     emailId:{
-        type:String
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
     },
     password:{
-        type:String
+        type:String,
+        required:true
     },
     age:{
-        type:Number
+        type:Number,
+        min:18
     },
     gender:{
-        type:String
-    }
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender data is not valid");
+            }
+        }
+    },
+    photoUrl:{
+        type:String,
+        default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLCexwutNZt0aSRMrVRLoXtex8XMNyWxjD4Q&s"
+    },
+    about:{
+        type:String,
+        default:"This is default description"
+    },
+    skills:{
+        type:[String]
+    },
+
+},{
+    timestamps:true,
 });
 
 module.exports = mongoose.model("User",userSchema);
