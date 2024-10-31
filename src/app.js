d2b61b8e78366b00ee71b3d24b9d257aa318b0f8
upendar9999 <upendar.reddy.musuku@accenture.com>
2024-10-31 17:40:23 +0530

    API level data sanitization

--- src/app.js
+++ src/app.js
@@ -66,10 +66,26 @@ app.delete("/user",async (req,res)=>{
 });
 
 //Update the data of the user
-app.patch("/user",async (req,res)=>{
-    const userId = req.body.userId;
+app.patch("/user/:userId",async (req,res)=>{
+    const userId = req.params?.userId;
     const data = req.body;
     try{
+        const ALLOWED_UPDATES = [
+            "photoUrl",
+            "about",
+            "gender",
+            "age",
+            "skills",
+        ];
+        const isUpdateAllowed = Object.keys(data).every((k)=>ALLOWED_UPDATES.includes(k));
+
+        if(!isUpdateAllowed){
+            throw new Error("Update not allowed");
+        }
+        if(data?.skills.length > 10){
+            throw new Error("Skills cannot be more than 10");
+        }
+        
          await User.findByIdAndUpdate({_id:userId},data,{
             returnDocument:"after",
             runValidators:true,
